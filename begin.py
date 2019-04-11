416068d12d90205ccf76ec80b6b8be376e0a9edf
root <root@DESKTOP-2QD4CQQ.localdomain>
2019-04-11 11:53:47 +0800

    解决scrapy crawl时中文输出乱码的问题

--- begin.py
+++ begin.py
@@ -2,4 +2,5 @@
 
 from scrapy import cmdline
 
-cmdline.execute("scrapy crawl subspider -o data.json".split())
+# cmdline.execute("scrapy crawl subspider -o data.json".split())
+cmdline.execute("scrapy crawl ebookspider -o ebookspider.json -s FEED_EXPORT_ENCODING=UTF-8".split())
